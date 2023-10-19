7c55042ec0e1ac29ba62fd3cf36e9e17f859cc72
Gin Vu Dang <gin.vudang@setel.com>
2023-10-19 09:32:32 +0700

    remove lite-youtube-embed

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,6 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     "~/assets/app.css",
-    'node_modules/lite-youtube-embed/src/lite-yt-embed.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
